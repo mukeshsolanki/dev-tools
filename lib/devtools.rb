a01d94df8bade558db46537bd8de63bdd95c981c
Mukesh <mukesh@keepworks.com>
2017-01-17 10:37:28 +0530

    Initial setup

--- lib/devtools.rb
+++ lib/devtools.rb
@@ -1,5 +1,23 @@
 require "devtools/version"
 
 module Devtools
-  # Your code goes here...
+  class DevToolsProject
+    def install (appname)
+        puts colorize("App to install #{appname}", "yellow")
+    end
+    def setupEnvironment (environment)
+        puts colorize("Environment to setup #{environment}", "yellow")
+    end
+    def colorize(text, color = "default", bgColor = "default")
+      colors = {"default" => "38","black" => "30","red" => "31","green" => "32","brown" => "33", "blue" => "34", "purple" => "35",
+                "cyan" => "36", "gray" => "37", "dark gray" => "1;30", "light red" => "1;31", "light green" => "1;32", "yellow" => "1;33",
+                "light blue" => "1;34", "light purple" => "1;35", "light cyan" => "1;36", "white" => "1;37"}
+      bgColors = {"default" => "0", "black" => "40", "red" => "41", "green" => "42", "brown" => "43", "blue" => "44",
+                  "purple" => "45", "cyan" => "46", "gray" => "47", "dark gray" => "100", "light red" => "101", "light green" => "102",
+                  "yellow" => "103", "light blue" => "104", "light purple" => "105", "light cyan" => "106", "white" => "107"}
+      color_code = colors[color]
+      bgColor_code = bgColors[bgColor]
+      return "\033[#{bgColor_code};#{color_code}m#{text}\033[0m"
+    end
+  end
 end
