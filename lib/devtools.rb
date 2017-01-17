d6c4d78f2245fa404efa31d5f6c81023c7ea79b2
Mukesh <mukesh@keepworks.com>
2017-01-17 11:08:37 +0530

    refactored base-setuo

--- lib/devtools.rb
+++ lib/devtools.rb
@@ -1,13 +1,43 @@
 require "devtools/version"
+require 'optparse'
+require 'ostruct'
 
 module Devtools
   class DevToolsProject
     def install (appname)
         puts colorize("App to install #{appname}", "yellow")
     end
+
     def setupEnvironment (environment)
         puts colorize("Environment to setup #{environment}", "yellow")
     end
+
+    def parseInput()
+      params = OpenStruct.new
+      OptionParser.new do |opt|
+        opt.on('-i', '--install APP_NAME', 'The flag used to indicate the tool that needs to be installed.') { |o| params.app_name = o }
+        opt.on('-s', '--setup ENVIRONMENT', 'The development environment to setup.') { |o| params.environment_name = o }
+      end.parse!
+
+      if params.app_name.to_s.strip.length == 0  && params.environment_name.to_s.strip.length == 0
+        puts colorize("Error: Missing option", "Red")
+        options = %x( devtools -h )
+        puts options
+        exit
+      else
+        return params
+      end
+    end
+
+    def checkIfBrewInstalled()
+      check_brew = %x( which brew )
+      if check_brew == "brew not found"
+        return false
+      else
+        return true
+      end
+    end
+
     def colorize(text, color = "default", bgColor = "default")
       colors = {"default" => "38","black" => "30","red" => "31","green" => "32","brown" => "33", "blue" => "34", "purple" => "35",
                 "cyan" => "36", "gray" => "37", "dark gray" => "1;30", "light red" => "1;31", "light green" => "1;32", "yellow" => "1;33",
